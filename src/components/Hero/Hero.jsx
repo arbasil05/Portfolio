6be165188dda61159dd968c95bff2f6157c0ac7b
arbasil05 <arbasil64.ab@gmail.com>
2025-11-30 16:06:19 +0530

    Added Parallax scroll to hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,49 @@
 import './Hero.css';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { useEffect } from 'react';
+
 
 const Hero = () => {
+    useEffect(() => {
+        const handleScroll = () => {
+            const cloud1 = document.querySelector('.cloud');
+            const cloud2 = document.querySelector('.cloud2');
+            const plane = document.querySelector('.Plane');
+            const plane2 = document.querySelector('.Plane2');
+
+            if (cloud1) {
+                cloud1.style.transform = `translateY(${window.scrollY * 0.15}px)`;
+            }
+            if (cloud2) {
+                cloud2.style.transform = `translateY(${window.scrollY * -0.45}px)`;
+            }
+
+            if(plane){
+                plane.style.transform = `translateY(${window.scrollY * 0.10}px)`;
+            }
+
+            if(plane2){
+                plane2.style.transform = `translateY(${window.scrollY * -0.25}px)`;
+            }
+
+
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+
     return (
         <>
             <div className='Hero-Container'>
+
+
+                <img src="./cloud1.png" className='cloud' />
+                <img src="./cloud1.png" className='cloud2' />
+                <img src="./Plane.png" className='Plane' />
+                <img src="./Plane2.png"  className='Plane2'/>
+
 
                 {/* Polaroid Image Card */}
                 <div className="Image-Card">
